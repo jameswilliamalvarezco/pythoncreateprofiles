60ebe7c54b8253d0ac129d825200bfb4b7136620
jameswilliamalvarezco <jameswilliamalvarezco@gmail.com>
2025-05-20 23:28:26 +0200

    Modified test_passes.py, unit test should now pass again

--- tests/test_passes.py
+++ tests/test_passes.py
@@ -4,8 +4,8 @@ class TestPasses(unittest.TestCase):
     def PipelineTest(self):
         self.assertTrue(True)
         
-    def test_fail(self):
-        self.assertTrue(False)
+    #def test_fail(self):
+    #    self.assertTrue(False)
 
 if __name__ == '__main__':
     unittest.main()
